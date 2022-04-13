6b1829a336e4cbf7349d6af82682ee59fe966af4
thj0309 <thj0309@cj.net>
2022-04-13 09:45:47 +0900

    상품 도메인 개발

--- src/main/java/jpabook/jpashop/domain/Item.java
+++ src/main/java/jpabook/jpashop/domain/Item.java
@@ -1,5 +1,6 @@
 package jpabook.jpashop.domain;
 
+import jpabook.jpashop.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -20,4 +21,23 @@ public abstract class Item {
 
     @ManyToMany(mappedBy="items")
     private List<Category> category;
+
+    /*
+     * 비즈니스 로직 *
+     */
+    /* 1. 재고 증가 */
+    public void addStock(int quantity){
+        this.stockQuantity += quantity;
+    }
+
+    /* 2. 재고 감소 */
+    public void removeStock(int quantity){
+        int restStock = this.stockQuantity -= quantity;
+
+        if (restStock < 0){
+            throw new NotEnoughStockException("need more stock");
+        }
+        this.stockQuantity = restStock;
+    }
+
 }
