8529ab8e5e852bd8227bc40f5e6b97f1f1ea7b1f
thj0309 <thj0309@cj.net>
2022-04-14 16:56:36 +0900

    주문 도메인 개발

--- src/main/java/jpabook/jpashop/domain/Order.java
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -1,6 +1,8 @@
 package jpabook.jpashop.domain;
 
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
@@ -8,9 +10,12 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
+import static javax.persistence.FetchType.LAZY;
+
 @Entity
 @Table(name="orders")
 @Getter @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Order {
     @Id @GeneratedValue
     @Column(name= "order_id")
@@ -25,7 +30,7 @@ public class Order {
     @OneToMany(mappedBy = "order")
     private List<OrderItem> orderItems = new ArrayList<>();
 
-    @OneToOne
+    @OneToOne(fetch = LAZY, cascade = CascadeType.ALL)
     @JoinColumn(name="delivery_id")
     private Delivery delivery;
 
@@ -33,4 +38,61 @@ public class Order {
 
     @Enumerated(EnumType.STRING)
     private OrderStatus status; // 주문상태
+
+    /* 연관관계 메서드 */
+    public void setMember(Member member){
+        this.member = member;
+
+    }
+
+    public void addOrderItem(OrderItem orderItem){
+        orderItems.add(orderItem);
+        orderItem.setOrder(this);
+    }
+
+    public void setDelivery(Delivery delivery){
+        this.delivery = delivery;
+        delivery.setOrder(this);
+    }
+
+    /* 비즈니스 로직 */
+    public static Order createOrder(Member member, Delivery delivery, OrderItem... orderItems ){
+        Order order = new Order();
+        order.setMember(member);
+        order.setDelivery(delivery);
+
+        for (OrderItem orderItem: orderItems){
+            order.addOrderItem(orderItem);
+        }
+        order.setStatus(OrderStatus.ORDER);
+        order.setOrderDate(LocalDateTime.now());
+
+        return order;
+    }
+
+    /* 주문 취소 */
+    public void cancel(){
+        if(delivery.getDeliveryStatus() == DeliveryStatus.COMP){
+            throw new IllegalStateException("이미 배송 완료 상품은 취소 불가능합니다.");
+        }
+        this.setStatus(OrderStatus.CANCEL);
+        for(OrderItem orderItem : orderItems){
+            orderItem.cancel();
+        }
+
+    }
+
+    /* 조회 로직 */
+    /* 1. 전체 주문 가격 조회 */
+    public int getTotalPrice(){
+        // lamdba 활용 문법
+        return orderItems.stream().mapToInt(OrderItem::getTotalPrice).sum();
+
+        // jdk8 이전 문법
+//        int totalPrice = 0;
+//        for (OrderItem orderItem : orderItems){
+//            totalPrice += orderItem.getTotalPrice();
+//        }
+//        return totalPrice;
+    }
 }
